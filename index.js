5296bb2f0b22b40adcfeee25d3657bf20e70525e
MHfoudari <eng.mohammed.alfoudari@gmail.com>
2023-05-25 15:47:14 +0300

    finished the task

--- index.js
+++ index.js
@@ -1,46 +1,104 @@
-const employeesJSON = require('./employees.json');
-const managersJSON = require('./managers.json');
-
-// Code Here
+const employeesJSON = require("./employees.json");
+const managersJSON = require("./managers.json");
 
 class Employee {
- constructor (name, position, yearJoined, salary)
- {
+  constructor(name, position, yearJoined, salary) {
     this.name = name;
-    this.position =  position;
+    this.position = position;
     this.yearJoined = yearJoined;
     this.salary = salary;
-     }
-        }
-        idBadge = () => {
-            return (`Shell
-            ${this.position}: ${this.name}`)
-        }
+  }
 
-        class Manager extends Employee {
-            constructor (name, position, yearJoined, salary, bonusPercentage) 
-        { this.bonusPercentage = bonusPercentage;
+  idBadge() {
+    let pos = ` ${this.position}: ${this.name}`;
+    return pos;
+  }
+}
+class Manager extends Employee {
+  //this.emp[]
+  constructor(name, position, yearJoined, salary, bonusPercentage) {
+    super(name, position, yearJoined, salary);
+    this.bonusPercentage = bonusPercentage;
+  } // constructor
 
-            super(name, position, yearJoined, salary);
-        }
-        salaryIncrease = (increaseAmount) => {
-            return increaseAmount = this.salary++
-        }
-    }
-     const employees = new Employee();
-     employees.map = []
-     const managers = new Manager();
-     managers.map = []
+  salaryIncrease(increaseAmount) {
+    this.salary = this.salary + increaseAmount;
+  }
 
-     logManagerInfo =() => {
-        this.name
-        "since" workingYears
-        "Bonus%"  bonusPercentage
-     }
-     getWorkingYears =() => {
-        return 
-     }
+  logManagerInfo() {
+    //   <name>
+    //  since <workingYears>
+    //  Bonus % <bonusPercentage></bonusPercentage>
+    console.log(this.name);
+    console.log(`since ${this.getWorkingYears()}`);
+    console.log(`Bonus % ${this.bonusPercentage} / ${this.bonusPercentage}`);
+  }
+  getWorkingYears() {
+    let year_worked = 2023 - this.yearJoined;
+    return year_worked;
+  }
+} // Manager
 
+const employees = employeesJSON.map((emp) => {
+  //console.log(emp);
+  return new Employee(emp.name, emp.position, emp.yearJoined, emp.salary);
+  //return emp;
+});
+const managers = managersJSON.map((manage) => {
+  return new Manager(
+    manage.name,
+    manage.position,
+    manage.yearJoined,
+    manage.salary,
+    manage.bonusPercentage
+  );
+});
+console.log(employees);
+employees.forEach((emp) => console.log(emp.idBadge()));
+const superHeroes = employees.filter((emp) => emp.position === "Developer");
+console.log(superHeroes);
 
+managers.map((manage) => {
+  if (manage.getWorkingYears() > 11) manage.salaryIncrease(1000);
+});
+console.log(managers);
 
+managers.forEach((manage) => manage.logManagerInfo());
 
+const seniorExecutives = managers.filter((manage) => {
+  if (manage.yearJoined < 2012) return manage.name;
+});
+console.log(seniorExecutives);
+
+const lowestBonus = managers.find((manage) => {
+  if (manage.bonusPercentage === 0.1) return manage;
+});
+console.log(lowestBonus);
+
+const promotionCandidate = employees.filter((emp) => {
+  if (emp.salary > 8000) return emp;
+});
+console.log(promotionCandidate);
+
+let highest = employees[0].salary;
+
+const mostPaidEmployee = employees.forEach((emp) => {
+  if (emp.salary > highest) highest = emp.salary;
+  return emp;
+});
+console.log(mostPaidEmployee);
+
+const totalSalaries = employees.reduce(
+  (total, salary1) => total + salary1.salary,
+  0
+);
+console.log(`the total salaries is ${totalSalaries}`);
+
+function compareNumbers(a, b) {
+  return a - b;
+}
+const employeesSorted = employees.map((emp) => {
+  return emp.salary;
+});
+employeesSorted.sort(compareNumbers);
+console.log(employeesSorted);
